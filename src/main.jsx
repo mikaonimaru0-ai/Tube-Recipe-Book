3c3fb01b6ea1dde16ce8f1aa19e2882c68654b0b
meowmeow-hue <2025402rith@aupp.edu.kh>
2026-07-08 16:25:56 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { DrinkProvider } from "./context/DrinkContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.PROD ? "/Tube-Recipe-Book" : "/"}>
+    <HashRouter>
       <DrinkProvider>
         <App />
       </DrinkProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
